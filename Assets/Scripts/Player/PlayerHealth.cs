cc56f8d58157b95db35009ec506806f0afb9af64
GonzaloBiagioni <gonzalobiagioni3@gmail.com>
2024-11-02 23:54:25 -0300

    Sistema HP Player

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+    public GameObject gameOverPanel;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        gameOverPanel.SetActive(false);
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnemyProjectile"))
+        {
+            TakeDamage(1);
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    private void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+}
